bc298dd0b6cd207bfe811e0b59eb16b9a31df2e3
Pavankumar Vengadesappa <48195977+PavankumarVengadesappa@users.noreply.github.com>
2019-04-30 16:31:51 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'files_comparision',         # How you named your package folder (MyLib)
+  packages = ['files_comparision'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Used to compare 2 files',   # Give a short description about your library
+  author = 'PAVANKUMAR VENGADESAPPA',                   # Type in your name
+  author_email = 'pavankumarvenkatesh8@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/PavankumarVengadesappa/files_comparision.py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/PavankumarVengadesappa/files_comparision.py',    # I explain this later on
+  keywords = ['COMPARE', 'FILES'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pip',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
